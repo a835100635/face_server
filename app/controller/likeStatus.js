aac5bd6e427f405a36be68dac65255c5f37f7b83
leonli <leonli@wezhuiyi.com>
2023-04-04 18:10:57 +0800

    feat: 点赞功能完善

--- app/controller/likeStatus.js
+++ app/controller/likeStatus.js
@@ -47,6 +47,43 @@ class LikeStatusController extends Controller {
 
     ctx.body = '';
   }
+
+  /**
+   * 取消点赞
+   */
+  async unlike() {
+    const { ctx } = this;
+    const { openid } = ctx.state.userInfo;
+    const { type, topicId, commentId } = ctx.request.body;
+
+    // 判断点赞类型
+    if (type === TOPIC && topicId) {
+      // 题目
+      // 判断题目是否存在
+      const topic = await ctx.service.topic.checkTopic({ value: topicId });
+      if (!topic) {
+        throw new BadRequestException('题目不存在');
+      }
+
+      // 判断是否已经点赞或者点踩
+      const exist = await ctx.service.likeStatus.checkLikeStatus({ userId: openid, topicId, type: TOPIC });
+
+      if (exist) {
+        // 已经点赞或者点踩 执行删除
+        await ctx.service.likeStatus.deleteLikeStatus({ userId: openid, topicId, type: TOPIC });
+      } else {
+        throw new BadRequestException('未点赞');
+      }
+    }
+
+    // TODO: 滞后开发
+    if (type === COMMENT && commentId) {
+      // 评论
+      // 判断评论是否存在
+    }
+
+    ctx.body = '';
+  }
 }
 
 module.exports = LikeStatusController;
